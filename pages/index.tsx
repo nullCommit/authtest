2079c31451952cf8dd592e0de3916ba75d53aab7
Allan <allangeek@gmail.com>
2022-04-19 18:45:59 -0300

    Cria form de autenticação

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    console.log(data);
+  }
+
   return (
-    <div className={styles.container}>
-      <input type='text' />
-    </div>
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input
+        type='email'
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <input
+        type='password'
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <button type='submit'>Entrar</button>
+    </form>
   );
 }
