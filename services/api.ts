507650648d0096ef92e382ec8ac820311104daf5
Allan <allangeek@gmail.com>
2022-04-25 16:06:45 -0300

    Cria lógica de logout automático

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../contexts/AuthContext';
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -78,7 +79,9 @@ api.interceptors.response.use(
         });
       } else {
         // desloga o usuário
+        signOut();
       }
     }
+    return Promise.reject(error);
   }
 );
