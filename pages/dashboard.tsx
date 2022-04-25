285b9e43a40a704c9599188796f9792662ff6198
Allan <allangeek@gmail.com>
2022-04-25 18:33:00 -0300

    Valida autenticação do lado do server (SSR)

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,16 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+
 import { AuthContext } from '../contexts/AuthContext';
+import { withSSRAuth } from '../utils/withSSRAuth';
+import { api } from '../services/apiClient';
+import { setupAPIClient } from '../services/api';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get('/me').then(response => console.log(response));
+  }, []);
 
   return (
     <div>
@@ -10,3 +18,14 @@ export default function Dashboard() {
     </div>
   );
 }
+
+export const getServerSideProps = withSSRAuth(async ctx => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+
+  console.log(response.data);
+
+  return {
+    props: {},
+  };
+});
